c478267025ff17d7ad4f7d593342c6ba5c008553
hidalga <51562547+hidalga@users.noreply.github.com>
2019-06-14 18:15:54 -0500

    Update fbtestbot.py

--- fbtestbot.py
+++ fbtestbot.py
@@ -13,7 +13,7 @@ def handle_verification():
     if request.args.get('hub.verify_token', '') == VERIFY_TOKEN:
         return request.args.get('hub.challenge', 200)
     else:
-        mensaje = input()
+        mensaje = raw_input()
         return mensaje
 
 
